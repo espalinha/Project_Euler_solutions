4c3b2eb2e680352d4f8cb21c861de3f3cf9835a3
Espala <matheusespalaor14@gmail.com>
2023-10-22 21:36:02 -0300

    Primeiro Exercício

---
+++ mult_of_3_5/Solution.java
@@ -0,0 +1,19 @@
+/*
+ * Matheus Rodrigues da Silva Espalaor
+ */
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        long soma = 0;
+        for(int i = 3; i < 1000; i++)
+        {
+            if(i % 3 == 0 || i % 5 == 0)
+            {
+                soma += i;
+            }
+        }
+        System.out.println(soma);
+    }
+}
